90619f27c76889a91afbe85e6548630514f46d1b
suryakala20 <suryakala257r@gmail.com>
2025-12-15 20:59:51 +0530

    Add files via upload

---
+++ RandomHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class RandomHelper
+{
+    private static Random random = new Random();
+    public static int randint(int min, int max)
+    {
+        return random.Next(min, max + 1);
+    }
+    public static double randdouble(int min, int max)
+    {
+        return min + random.NextDouble() * (max - min);
+    }
+}
---
+++ RandomProgram.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int randomInt = RandomHelper.randint(1, 10);
+        double randomDouble = RandomHelper.randdouble(1, 10);
+        Console.WriteLine("Random Integer (1 to 10): " + randomInt);
+        Console.WriteLine("Random Double (1 <= x < 10): " + randomDouble);
+    }
+}
